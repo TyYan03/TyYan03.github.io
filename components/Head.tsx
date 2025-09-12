35b10126dc30661ef8f5760b3a6ee4d80d91b590
Tyler Yan <tylery202@gmail.com>
2025-09-12 00:57:35 -0400

    update og:image value

--- components/Head.tsx
+++ components/Head.tsx
@@ -21,8 +21,8 @@ const CustomHead = ({ title }: CustomHeadProps) => {
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta property="og:image" content="/main/main.png" />
+      <meta property="og:url" content="https://tyyan03.github.io" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
